d0b8a508fc1abef509847025dd40886c94ffcbe7
Cristhian <yurday93@hotmail.com>
2021-06-17 16:47:15 -0500

    Declaración de variables y complementación del comportamiento de los AR y entidades

--- src/main/java/cine/Sala.java
+++ src/main/java/cine/Sala.java
@@ -1,4 +1,24 @@
 package cine;
 
-public class Sala {
+import cine.values.Capacidad;
+import cine.values.NombreSala;
+import cine.values.SalaId;
+import co.com.sofka.domain.generic.Entity;
+
+import java.util.Objects;
+
+public class Sala extends Entity <SalaId> {
+
+    private NombreSala nombreSala;
+    private Capacidad capacidad;
+
+    public Sala (SalaId entityId, NombreSala nombreSala, Capacidad capacidad){
+        super(entityId);
+        this.nombreSala = nombreSala;
+        this.capacidad = capacidad;
+    }
+
+    public void modificarNombreDeSala(NombreSala nombreSala){
+        this.nombreSala = Objects.requireNonNull(nombreSala);
+    }
 }
--- src/main/java/cine/Tiquete.java
+++ src/main/java/cine/Tiquete.java
@@ -1,4 +1,25 @@
 package cine;
 
-public class Tiquete {
+import cine.values.NombreDeVendedor;
+import cine.values.TiqueteId;
+import co.com.sofka.domain.generic.Entity;
+
+import java.util.Objects;
+
+public class Tiquete extends Entity <TiqueteId> {
+
+    private NombreDeVendedor nombreDeVendedor;
+
+    public Tiquete (TiqueteId entityId, NombreDeVendedor nombreDeVendedor){
+        super(entityId);
+        this.nombreDeVendedor = nombreDeVendedor;
+    }
+
+    public void actualizarNombreDeVendedor(NombreDeVendedor nombreDeVendedor){
+        this.nombreDeVendedor = Objects.requireNonNull(nombreDeVendedor);
+    }
+
+    public NombreDeVendedor nombreDeVendedor(){
+        return nombreDeVendedor;
+    }
 }
